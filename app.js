32b03c7affa2db6ee3f1afb87459d009989c2c9b
anthonynarine <anarine83@gmail.com>
2022-11-12 16:41:24 -0500

    Prblems 6-10 completed

--- app.js
+++ app.js
@@ -124,21 +124,21 @@ console.log("Mexican Foods: ", mexicanFood);
 //1. Create a function that will return all dishes with the cuisine type of "vegetarian"
 //Filter
 const vegetarianDishes = dishes.filter(vegeDishe => vegeDishe.cuisine.includes("Vegetarian"));
-console.log("Vegetarian Dishes: ", vegetarianDishes)
+console.log("Vegetarian Dishes: ", vegetarianDishes);
 
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
 
 const italianDishes = dishes.filter(italianFoods => (
   italianFoods.cuisine.includes("Italian") && italianFoods.servings > 5
-))
+));
 console.log("Italian Dishes That Server more than 5", italianDishes);
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 const idEqualServingCount = dishes.filter (dish =>{
   results = dish.id === dish.servings;
   return results;
-})
+});
 
 
 //4. Create a function that will return only dishes whose serving count is even.
@@ -146,7 +146,7 @@ const idEqualServingCount = dishes.filter (dish =>{
 const evenServings = dishes.filter(isEven => {
   results1 = isEven.servings % 2 === 0;
   return results1;
-})
+});
 
 
 //5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
@@ -155,7 +155,7 @@ const caprese = dishes.filter(tommatoAndCheese => {
  dishContains =  tommatoAndCheese.ingredients.includes("tomato") &&
 tommatoAndCheese.ingredients.includes("cheese");
 return dishContains;
-})
+});
 
 
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
@@ -167,16 +167,37 @@ const cuisineType = dishes.filter(type => type.cuisine)
 
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map
+const cuisineName = dishes.map((el) => {
+  return `${el.cuisine} ${el.name}`;
+});
 
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
+const veggies = cuisineName.filter((element) =>{
+  return element.includes("Vegetarian")
+});
 
 // <<<<<<<<<<<<<<<<< BONUS <<<<<<<<<<<<<<<<<
 //9. Create a function that will return dishes whose ingredients array INCLUDES "chickpea".
 //Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
 //Filter
+const containsChickpea= dishes.filter((element1) =>(
+  element1.ingredients.includes("chickpea")
+));
 
 //10. Create a function that will return the total serving count of all dishes.
 //Must use Reduce, not a loop.
+const serving = dishes.map((element2) =>{
+  const totalServings = element2.servings;
+  return totalServings;
+});
+const totalServingOfAllDishes = serving.reduce((total, currentVal) => {
+  return total + currentVal;
+})
 
 //11. Create a function that will return an array of any objects that do not share a cuisine type with any other objects.
+dishes.filter((item) =>{
+  if(item.cuisine !== item.cuisine) return 
+})
+
+
